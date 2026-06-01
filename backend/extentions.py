22b8289623fddf49abbe8c01e6ec8b9ff78e5d9e
essentialbusiness6-wq <essentialbusiness6@gmail.com>
2026-06-01 07:08:20 -0400

    Update extentions.py

--- backend/extentions.py
+++ backend/extentions.py
@@ -2,7 +2,7 @@ from flask_socketio import SocketIO
 
 socketio = SocketIO(
     cors_allowed_origins="*",
-    async_mode="eventlet",
+    async_mode="gevent",
     manage_session=True,
     cors_credentials=True
 )
