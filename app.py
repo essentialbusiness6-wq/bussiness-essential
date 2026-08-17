9cc0671bf85bb5f2ff47e701d3c28974c735e0d5
essentialbusiness6-wq <essentialbusiness6@gmail.com>
2026-08-17 07:42:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ def whatsapp_webhook():
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template("404.html"), 404
+    return render_template("users/404.html"), 404
         
 @app.route("/test-notification")
 def test_notification():
