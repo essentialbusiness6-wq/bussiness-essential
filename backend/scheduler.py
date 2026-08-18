8e7d43d50cb106a1d5248be60d1fe609b060fd12
essentialbusiness6-wq <essentialbusiness6@gmail.com>
2026-08-18 14:22:29 -0400

    Update scheduler.py

--- backend/scheduler.py
+++ backend/scheduler.py
@@ -8,15 +8,14 @@ scheduler = BackgroundScheduler()
 def scheduled_job():
     print("Running Schedueled Job...")
 
-    with app.app_context():
-        print("Checking overdue Invoices...")
-        auto_check_overdue_invoices()
+    print("Checking overdue Invoices...")
+    auto_check_overdue_invoices()
 
-        print("Checking Expired Subsriptions...")
-        process_expired_subscriptions()
+    print("Checking Expired Subsriptions...")
+    process_expired_subscriptions()
 
-        print("Checking Invoice Due Notifications...")
-        process_invoice_due_notifications()
+    print("Checking Invoice Due Notifications...")
+    process_invoice_due_notifications()
         
     print("Scheduled Job Finished...")
 
