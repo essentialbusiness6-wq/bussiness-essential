f3610dc7af0929f5b12e61b0d639769a5f208209
essentialbusiness6-wq <essentialbusiness6@gmail.com>
2026-08-18 14:02:27 -0400

    Update scheduler.py

--- backend/scheduler.py
+++ backend/scheduler.py
@@ -6,17 +6,26 @@ import os
 scheduler = BackgroundScheduler()
 
 def scheduled_job():
+    print("Running Schedueled Job...")
+
     with app.app_context():
+        print("Checking overdue Invoices...")
         auto_check_overdue_invoices()
+
+        print("Checking Expired Subsriptions...")
         process_expired_subscriptions()
+
+        print("Checking Invoice Due Notifications...")
         process_invoice_due_notifications()
+        
+    print("Scheduled Job Finished...")
+
 
 scheduler.add_job(
     scheduled_job,
     trigger='interval',
-    hours=1
+    minutes=1
 )
 
-if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
-    print("Started")
-    scheduler.start()
+print("Started")
+scheduler.start()
